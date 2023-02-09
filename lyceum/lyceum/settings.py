823b2d55c3e12f2a1cb60349a670614347c71d5b
Bananad47 <demid.shumakher@gmail.com>
2023-02-09 18:57:33 +0500

    i9

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -27,9 +27,8 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = os.getenv("DEBUG")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
 
 
 # Application definition
