940c71b8f39335a82b237f940d169abddd75b121
Bananad47 <demid.shumakher@gmail.com>
2023-02-12 02:30:15 +0500

    Correct_lint

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase, Client
 
 
 class StaticURLTests(TestCase):
-
     def test_homepage_endpoint(self):
         response = Client().get("/")
         self.assertEqual(response.status_code, 200)
