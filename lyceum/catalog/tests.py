dcae1b4c1e89f9023d768040427265818e9a11b6
Bananad47 <demid.shumakher@gmail.com>
2023-02-12 03:51:58 +0500

    Black fixed

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -5,7 +5,7 @@ class StaticURLTests(TestCase):
     def test_item_list_endpoint(self):
         response = Client().get("/")
         self.assertEqual(response.status_code, 200)
-    
+
     def test_item_detail_endpoint(self):
         response = Client().get("/catalog/55")
         self.assertEqual(response.status_code, 200)
