7a9d4bf935331b09de9bc6bd1b2c2fbc08fb6f12
Bananad47 <demid.shumakher@gmail.com>
2023-02-14 02:47:46 +0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA Ритуал окончен

--- lyceum/middleware/middleware.py
+++ lyceum/middleware/middleware.py
@@ -9,15 +9,15 @@ class SimpleMiddleware:
         self.state = settings.REVERSE_RUSSIAN_WORDS
 
     def __call__(self, request):
-
         response = self.get_response(request)
         self.cnt += 1
 
         if self.state and self.cnt >= 10:
             self.cnt = 0
             response_text = response.getvalue().decode("utf-8")
-            reversed_text = re.sub("[а-яА-Я]+", lambda m: m.group(0)[::-1],
-                                   response_text)
+            reversed_text = re.sub(
+                "[а-яА-Я]+", lambda m: m.group(0)[::-1], response_text
+            )
             response.content = reversed_text
 
         return response
