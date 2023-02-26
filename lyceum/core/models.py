e7f5fd1cb5e91633bb850b8146c99d458b9889a7
Bananad47 <demid.shumakher@gmail.com>
2023-02-26 16:52:26 +0500

    change validator

--- lyceum/core/models.py
+++ lyceum/core/models.py
@@ -1,18 +1,20 @@
 import re
-from functools import wraps
 
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.deconstruct import deconstructible
 
 
-def custom_validator(*words):
-    @wraps(custom_validator)
-    def validate(value):
-        for word in words:
+@deconstructible
+class custom_validator(object):
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self, value):
+        for word in self.words:
             if re.findall(r"\b({})\b".format(word), value.lower()) == []:
                 raise ValidationError(f"Ваш текст не содержит слова {word}!")
-
-    return validate
+        return self
 
 
 class AbstractionModel(models.Model):
