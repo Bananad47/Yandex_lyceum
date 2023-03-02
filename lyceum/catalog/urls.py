14b294839360b83c1103396bddda070bc69bc470
Bananad47 <demid.shumakher@gmail.com>
2023-03-02 16:59:21 +0500

    homepage

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -4,8 +4,10 @@ from catalog import converters, views
 
 register_converter(converters.PositiveNumber, "id")
 
+app_name = "catalog"
+
 urlpatterns = [
-    path("", views.item_list),
+    path("", views.item_list, name="item_list"),
     path("<int:item_id>/", views.item_detail),
     path("convector/<id:product_id>/", views.product_conventer_page),
     re_path(r"^re/(?P<number>[1-9][0-9]+|[1-9])/$", views.regular),
--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     BASE_DIR / "static_dev",
 ]
 
